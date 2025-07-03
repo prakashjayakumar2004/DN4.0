a797b15ca089fa395f2f0327817ee9ec525f10b4
prakashjayakumar2004 <162820285+prakashjayakumar2004@users.noreply.github.com>
2025-07-03 19:17:44 +0530

    Add files via upload

---
+++ Week 3_Spring Core and Maven/Exercise 2 - Implementing Dependency Injection/src/main/java/com/example/librarymanagement/MainApp.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagement;
+
+import com.example.librarymanagement.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService service = context.getBean("bookService", BookService.class);
+        service.issueBook("Java Programming Essentials");
+    }
+}
